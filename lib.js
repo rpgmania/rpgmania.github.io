8cb1aa3168dcaabe89b754d4997e9f568c063824
Aizen <163570819+rpgmania@users.noreply.github.com>
2024-03-18 17:27:11 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -1,4 +1,3 @@
-// Define Stimulus controllers
 const application = Stimulus.Application.start();
 
 application.register("counter", class extends Stimulus.Controller {
@@ -65,37 +64,38 @@ application.register("rolldmg", class extends Stimulus.Controller {
   static targets = ["value"];
 
   roll() {
-     let diceRoll1 = Math.floor(Math.random() * 8) + 1;
-  let diceRoll2 = Math.floor(Math.random() * 8) + 1;
-  let newValue = diceRoll1 + diceRoll2 + 1;
-  this.valueTarget.textContent = newValue;
+    let diceRoll1 = Math.floor(Math.random() * 8) + 1;
+    let diceRoll2 = Math.floor(Math.random() * 8) + 1;
+    let newValue = diceRoll1 + diceRoll2 + 1;
+    this.valueTarget.textContent = newValue;
   }
 
   rolltrix() {
     let diceRoll = Math.floor(Math.random() * 6) + 1;
-  let newValue = diceRoll + 3;
-  this.valueTarget.textContent = newValue;
+    let newValue = diceRoll + 3;
+    this.valueTarget.textContent = newValue;
   }
 
   rollSkel() {
-    let diceRoll1 = Math.floor(Math.random() * 6) +1;
-  let diceRoll2 = Math.floor(Math.random() * 6) +1;
-  let diceRoll3 = Math.floor(Math.random() * 6) +1;
-  let newValue = diceRoll1 + diceRoll2 + diceRoll3 + 2;
+    let diceRoll1 = Math.floor(Math.random() * 6) + 1;
+    let diceRoll2 = Math.floor(Math.random() * 6) + 1;
+    let diceRoll3 = Math.floor(Math.random() * 6) + 1;
+    let newValue = diceRoll1 + diceRoll2 + diceRoll3 + 2;
+    this.valueTarget.textContent = newValue;
   }
 
   rollDrain() {
-   let diceRoll = Math.floor(Math.random() * 10) + 1;
-  let newValue = diceRoll + 4;
-  this.valueTarget.textContent = newValue;
-
+    let diceRoll = Math.floor(Math.random() * 10) + 1;
+    let newValue = diceRoll + 4;
+    this.valueTarget.textContent = newValue;
   }
 
   rollThief() {
-  let diceRoll1 = Math.floor(Math.random() * 6) +1;
-  let diceRoll2 = Math.floor(Math.random() * 6) +1;
-  let diceRoll3 = Math.floor(Math.random() * 6)+1;
-  let newValue = diceRoll1 + diceRoll2 + diceRoll3 + 1;
+    let diceRoll1 = Math.floor(Math.random() * 6) + 1;
+    let diceRoll2 = Math.floor(Math.random() * 6) + 1;
+    let diceRoll3 = Math.floor(Math.random() * 6) + 1;
+    let newValue = diceRoll1 + diceRoll2 + diceRoll3 + 1;
+    this.valueTarget.textContent = newValue;
   }
 });
 
@@ -103,27 +103,27 @@ application.register("attack", class extends Stimulus.Controller {
   static targets = ["value"];
 
   atk() {
-    let newValue = parseInt(Math.floor(Math.random() * 20) + 1);
+    let newValue = Math.floor(Math.random() * 20) + 1;
     this.valueTarget.textContent = newValue;
   }
 
   atkSkel() {
-    let newValue = parseInt(Math.floor(Math.random() * 20));
+    let newValue = Math.floor(Math.random() * 20) + 1;
     this.valueTarget.textContent = newValue;
   }
 
   atkDrain() {
-    let newValue = parseInt(Math.floor(Math.random() * 20) + 3);
+    let newValue = Math.floor(Math.random() * 20) + 3;
     this.valueTarget.textContent = newValue;
   }
 
   atkThief() {
-    let newValue = parseInt(Math.floor(Math.random() * 20) + 3);
+    let newValue = Math.floor(Math.random() * 20) + 3;
     this.valueTarget.textContent = newValue;
   }
 
   atktrix() {
-    let newValue = parseInt(Math.floor(Math.random() * 20) + 1);
+    let newValue = Math.floor(Math.random() * 20) + 1;
     this.valueTarget.textContent = newValue;
   }
 });
