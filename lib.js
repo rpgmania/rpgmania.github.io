a37f9f9620a0b382bd587a69794822331e4a8e35
Aizen <163570819+rpgmania@users.noreply.github.com>
2024-03-19 17:59:01 +0100

    Update lib.js

--- lib.js
+++ lib.js
@@ -145,11 +145,4 @@ application.register("attack", class extends Stimulus.Controller {
     this.valueTarget.textContent = newValue;
   }
 });
- document.addEventListener('DOMContentLoaded', function () {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const mobileMenu = document.querySelector('.mobile-menu');
-
-    menuToggle.addEventListener('click', function () {
-      mobileMenu.classList.toggle('active');
-    });
-  });
+ 
